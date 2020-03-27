b132f4ff1c163ac4f9c5cb7b2e98a2a314f82c4c
substack <substack@bits.coop>
2020-03-26 22:08:20 -1000

    failing case with extra zeros in vectors

--- tests/builtins.rs
+++ tests/builtins.rs
@@ -65,3 +65,57 @@ fn arrays() -> Result<(),Error> {
   assert_eq![<[u16;3]>::count_from_bytes(&[0,6,0,7,0,8,200,201,202,203,204])?, 6];
   Ok(())
 }
+
+#[test]
+fn vectors() -> Result<(),Error> {
+  {
+    let xs: Vec<u8> = vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13];
+    assert_eq![
+      xs.to_bytes()?,
+      vec![14,0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+    ];
+    assert_eq![
+      <Vec<u8>>::from_bytes(&xs.to_bytes()?)?,
+      (15,vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13])
+    ];
+  }
+  {
+    let xs: Vec<u8> = (0..200).map(|x| {
+      (((x*137+24) as u16)%256) as u8
+    }).collect();
+    assert_eq![
+      <Vec<u8>>::from_bytes(&xs.to_bytes()?)?,
+      (xs.len()+2,xs)
+    ];
+  }
+  Ok(())
+}
+
+#[test]
+fn nested() -> Result<(),Error> {
+  {
+    let xs: Vec<u8> = (0..200).map(|x| {
+      (((x*137+24) as u16)%256) as u8
+    }).collect();
+    let len = xs.len();
+    let copy = xs.clone();
+    assert_eq![
+      <(u8,u32,u16,Vec<u8>)>::from_bytes(&(4u8,5u32,6u16,xs).to_bytes()?)?,
+      (len+2+7,(4u8,5u32,6u16,copy))
+    ];
+  }
+  {
+    let xs = vec![
+      211, 245, 162, 214, 25, 238, 66, 38, 34, 37, 155, 210, 230, 76, 17, 182,
+      128, 200, 112, 118, 77, 62, 24, 32, 201, 100, 198, 21, 244, 182, 52, 81,
+      158, 55, 43, 0, 61, 115, 92, 4, 6, 80, 197, 51, 241, 19, 131, 140, 238,
+      147, 19, 43, 149, 21, 93, 56, 190, 249, 106, 15, 20, 166, 69, 201, 121,
+      36, 111, 180
+    ];
+    assert_eq![<Vec<u8>>::from_bytes(&xs.to_bytes()?)?, (69,xs.clone())];
+    type T = (((f32,f32),(f32,f32),f32),Vec<u8>);
+    let obj: T = (((-0.79,-0.68),(0.46,0.46),40.73), xs.clone());
+    assert_eq![T::from_bytes(&obj.to_bytes()?)?, (89,obj)];
+  }
+  Ok(())
+}
